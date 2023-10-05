719ecf101c535b752b75dafdc5f5238794652e1d
Max <143605086+maxim513@users.noreply.github.com>
2023-10-05 17:14:14 +0200

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,33 @@
+
+    const data = generateRandomData();
+
+    for (let i = 0; i < 10; i++) {
+        data.push(Math.floor(Math.random() * 100) + 1);
+        console.log("Random Number: " + item);
+main();
+    });
+    return data;
+    }
+
+function generateRandomData() {
+    data.forEach(item => {
+}
+function main() {
+    const data = [];
+}    }
+}
+function generateRandomData() {
+    const data = [];
+    return data;
+
+
+    data.forEach(item => {
+}
+main();
+function main() {
+
+    });
+        data.push(Math.floor(Math.random() * 100) + 1);
+    for (let i = 0; i < 10; i++) {
+    const data = generateRandomData();
+        console.log("Random Number: " + item);
