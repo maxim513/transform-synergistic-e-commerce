1ef11f30f76deeec9b8cbba6f2d8fb6cee5284ed
Max <143605086+maxim513@users.noreply.github.com>
2023-10-08 05:38:02 +0200

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,26 +1,68 @@
-    for item in data:
-        print(f"Random Number: {item}")
+}
+    public static void main(String[] args) {
+            data[i] = (int) (Math.random() * 100) + 1;
+    }
+        for (int i = 0; i < 10; i++) {
+        for (int item : data) {
 
+public class RandomDataGenerator {
+        }
+        int[] data = new int[10];
+            System.out.println("Random Number: " + item);
+
+        }
 def generate_random_data():
+    data = [random.randint(1, 100) for _ in range(10)]
+
+if __name__ == "__main__":
+def main():
+        print(f"Random Number: {item}")
+import random
+    main()
+
+
+    return data
+    for item in data:
+    data = generate_random_data()    for item in data:
+def generate_random_data():
+def main():
 
 
     data = generate_random_data()
-def main():
-import random
-if __name__ == "__main__":
-    data = [random.randint(1, 100) for _ in range(10)]
-    return data
-
-    main()    data = generate_random_data()
-    data = [random.randint(1, 100) for _ in range(10)]
-import random
         print(f"Random Number: {item}")
 
-    for item in data:
-def generate_random_data():
-    return data
+import random
+
 if __name__ == "__main__":
     main()
+    data = [random.randint(1, 100) for _ in range(10)]
+    return data    });
+        data.push(Math.floor(Math.random() * 100) + 1);
+main();
+    const data = [];
+    data.forEach(item => {
+}
+}
+function generateRandomData() {
+    return data;
+    }
+
+
+function main() {
+        console.log("Random Number: " + item);
+    for (let i = 0; i < 10; i++) {
+    const data = generateRandomData();
 def main():
+if __name__ == "__main__":
+        print(f"Random Number: {item}")
+    main()
+    for item in data:
+
+import random
+    return data
+def generate_random_data():
+    data = [random.randint(1, 100) for _ in range(10)]
 
 
+
+    data = generate_random_data()
