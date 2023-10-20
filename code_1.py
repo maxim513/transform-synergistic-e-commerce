e31d1a48b639212c63635e45ead1751501a53f9b
Max <143605086+maxim513@users.noreply.github.com>
2023-10-20 20:27:18 +0200

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,64 +1,40 @@
-            data[i] = (int) (Math.random() * 100) + 1;
-            System.out.println("Random Number: " + item);
-        }
-        int[] data = new int[10];
-
-        }
-        for (int i = 0; i < 10; i++) {
-public class RandomDataGenerator {
-
-        for (int item : data) {
-    public static void main(String[] args) {
-}
-    }def generate_random_data():
-        print(f"Random Number: {item}")
-
-
+    data = generate_random_data()
 import random
-if __name__ == "__main__":
+
     for item in data:
-    data = [random.randint(1, 100) for _ in range(10)]
-    data = generate_random_data()
-    return data
-
-    main()
-
-def main():    for item in data:
-import random
-
-
-
-
-    return data
-    data = [random.randint(1, 100) for _ in range(10)]
-    main()
-def generate_random_data():
-        print(f"Random Number: {item}")
 if __name__ == "__main__":
-    data = generate_random_data()
-def main():    main()
-import random
-
-def generate_random_data():
-if __name__ == "__main__":
-
+    main()
     return data
+
 
 def main():
-        print(f"Random Number: {item}")
+
     data = [random.randint(1, 100) for _ in range(10)]
+        print(f"Random Number: {item}")
+def generate_random_data():    data = [random.randint(1, 100) for _ in range(10)]
+if __name__ == "__main__":
+        print(f"Random Number: {item}")
+    main()
+
+    for item in data:
+
+    return data
+import random
+    data = generate_random_data()
+def generate_random_data():
+
+
+def main():
+
+
+import random
+if __name__ == "__main__":
     for item in data:
     data = generate_random_data()
-            data[i] = (int) (Math.random() * 100) + 1;
-        }
-        for (int i = 0; i < 10; i++) {
-        for (int item : data) {
-    }
-        int[] data = new int[10];
-public class RandomDataGenerator {
-            System.out.println("Random Number: " + item);
-    public static void main(String[] args) {
-        }
 
-
-}
+    return data
+        print(f"Random Number: {item}")
+def main():
+    data = [random.randint(1, 100) for _ in range(10)]
+    main()
+def generate_random_data():
